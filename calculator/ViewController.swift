8fcda291d57b9f6c86f22c8b47841c6dd592cb2a
snig-17 <snigdha.17.tiwari@icloud.com>
2024-08-01 17:25:22 +0530

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Snigdha Tiwari  on 01/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
